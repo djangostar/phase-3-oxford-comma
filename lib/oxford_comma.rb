e211671457e4e66da983679fb727bcef8c45e30d
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-17 20:45:28 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(arr)
+    if arr.length == 1
+        return "#{arr[0]}"
+    elsif arr.length == 2
+        return arr.join(" and ")
+    elsif arr.length >= 3
+        arr[-1] = "and #{arr[-1]}"
+        return arr.join(", ")
+    end
 end
+
+
+
