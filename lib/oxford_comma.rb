0bac311afd4f94f842978ea34c161eb71a30f6f3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-17 20:49:00 -0700

    Refractored

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,21 @@
 require 'pry'
 
+# def oxford_comma(arr)
+#     if arr.length == 1
+#         return "#{arr[0]}"
+#     elsif arr.length == 2
+#         return arr.join(" and ")
+#     elsif arr.length >= 3
+#         arr[-1] = "and #{arr[-1]}"
+#         return arr.join(", ")
+#     end
+# end
+
+## REFRACTOR
+
 def oxford_comma(arr)
-    if arr.length == 1
-        return "#{arr[0]}"
-    elsif arr.length == 2
-        return arr.join(" and ")
-    elsif arr.length >= 3
-        arr[-1] = "and #{arr[-1]}"
-        return arr.join(", ")
-    end
+    return arr.join(" and ") if arr.length < 3
+
+    arr[-1] = "and #{arr[-1]}"
+    arr.join(", ")
 end
-
-
-
